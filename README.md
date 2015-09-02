c97d7546ad9afaff24a5d59398ce0e7bf1d23c23
Barbara Compagnoni <compagnb@newschool.edu>
2015-09-02 13:58:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# data-vis-info-aethetics
---
+++ week1exercise/sketch.js
@@ -0,0 +1,11 @@
+// global variables
+
+// set up function
+function setup() {
+  createCanvas(windowWidth, 100);
+}
+
+function draw() {
+  ellipse(mouseX, 50, 50);
+  
+}
